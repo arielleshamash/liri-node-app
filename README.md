825b0be06fbdb63eeb20d04fdd70a1349d43b4d2
Arisham <arielleshamash@gmail.com>
2019-06-18 23:02:57 -0700

    working bot

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# liri-node-app
+# Liri Bot
+---
+
+### Description & Requirements
+---
+Liri is a command line application that takes user commands and queries from the command line and returns data from API's. liri.js can take utilize the following commands:
+
+Commands | Function
+---------|---------
+concert-this | uses the **bandsintown** API to take a band name from the user and returns that bands next concert
+![concert](/images/concert-this.png)
+spotify-this | uses the **spotify** API to take a song name from the user and returns the artist, song name, spotify-link and album 
+![spotify](/images/spotify-this-song.png)
+movie-this | uses the **OMDB** API to take a movie name and returns the name, cast, release year, IMDB and Rotten Tomatoes rating, country of origin, language and plot 
+![movie](/images/movie-this.png)
+do-this | uses the built in **readFile()** method to access data from a prepopulated .txt file and return its information as a command/search query.
--- liri.js
+++ liri.js
@@ -8,16 +8,16 @@ var request = require("request");
 var moment = require("moment");
 moment().format();
 
-var spotify = new spotify(keys.spotify);
-var omdb = new omdb(keys.omdb);
-var bandsintown = new bandsintown(keys.bandsintown);
+var spotify = new Spotify(keys.spotify);
+var omdb = (keys.omdb);
+var bandsintown = (keys.bandsintown);
 
-var userInput = process.argv[2];
-var userQuery = process.argv[3];
+var command = process.argv[2];
+var userInput = process.argv.slice(3).join(" ");
 
 //find concerts based on user inputs
-function concertThis(band) {
-    var queryURL = "https://rest.bandsintown.com/artists/" + bandQuery + "/events?app_id=codingbootcamp#";
+function concertThis() {
+    var queryURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp#";
 
     request(queryURL, function (error, response, body) {
         //if there is no error
@@ -27,7 +27,7 @@ function concertThis(band) {
             //loop through data
             if (userBand.length > 0) {
                 for (i = 0; i < 1; i++) {
-                    console.log("Artist: " + userBand[i].lineup[0] + "/nVenue: " + userBand[i].venue.name + "/nCity: " + userBand[i].venue.city);
+                    console.log("Artist: " + userBand[i].lineup[0] + "\nVenue: " + userBand[i].venue.name + "\nCity: " + userBand[i].venue.city);
 
                     //moment.js format the date and time of the concert
                     var concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY hh:00 A");
@@ -42,11 +42,82 @@ function concertThis(band) {
 
 };
 
-function spotifyThis(music) {
+function spotifyThisSong() {
     //if no search input, default to ace of base
-    if (!userQuery) {
-        userQuery = "the sign ace of base"
+    if (!userInput) {
+        userInput = "the sign ace of base"
     };
 
+    spotify.search({
+        type: "track",
+        query: userInput,
+        limit: 3
+    }, function (error, data) {
+        if (error) {
+            console.log("Error: " + error);
+        }else {
+
+            var spotifyArr = data.tracks.items;
+            for (i=0; i < spotifyArr.length; i++) {
+
+            //display spotify results
+            var spotifyResult = data.tracks.items[i];
+            console.log("Artist: " + spotifyResult.artists[0].name + "\nSong: " + spotifyResult.name + "\nLink: " + spotifyResult.external_urls.spotify + "\nAlbum: " + spotifyResult.album.name);
+            };
+        };
+    });
 };
 
+function movieThis() {
+    //if no user input default to Mr. Nobody
+    if (!userInput) {
+        userInput = "Mr. Nobody";
+    }
+
+    var queryURL = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+    request(queryURL, function (error, response, body) {
+
+        //if there is no error show results
+        if (!error && response.statusCode === 200) {
+            var movieSearch = JSON.parse(body);
+            console.log("Movie Title: " + movieSearch.Title + "\nYear: " + movieSearch.Year + "\nIMBD Rating: " + movieSearch.imdbRating + "\nRotten Tomatoes Rating: " + movieSearch.Ratings[1].Value + "\nCountry: " + movieSearch.Country + "\nLanguage: " + movieSearch.Language + "\nPlot: " + movieSearch.Plot + "\nMain Actors: " + movieSearch.Actors)
+        };
+    });
+};
+
+
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+
+        var dataArray = data.split(",");
+
+        command = dataArray[0];
+        userInput = dataArray[1];
+    });
+};
+
+function inputCommands(command, userInput) {
+    switch (command) {
+        case "concert-this":
+            concertThis();
+            break;
+        case "spotify-this-song":
+            spotifyThisSong();
+            break;
+        case "movie-this":
+            movieThis();
+            break;
+        case "do-what-it-says":
+            doWhatItSays(userInput);
+            break;
+        default:
+            console.log("I don't understand");
+            break;    
+    };
+};
+
+inputCommands(command, userInput);
